c558514d2e9cbf7ae8e7094aaf310dd9212d9f0b
Rodrigo Erico <rodrigoerico25@gmail.com>
2024-01-28 00:09:46 -0300

    returns an exception

--- learning-kotlin/src/functions/vararg.kt
+++ learning-kotlin/src/functions/vararg.kt
@@ -2,6 +2,7 @@ package functions
 
 fun main() {
     println(averageGrade(43534543,"Rodrigo",9f, 566, true, 3.5f, "Melancia", 4.5f, 7.8f))
+    nothing()
 }
 
 fun <K, T>averageGrade(anyValue: Any,name: K, vararg grades: T): Float {
@@ -12,4 +13,13 @@ fun <K, T>averageGrade(anyValue: Any,name: K, vararg grades: T): Float {
         }
     }
     return (sum / grades.size)
+}
+
+/*
+Pode utilizar Nothing para representar o valor “a que nunca existe”:
+por exemplo, se uma função tiver o tipo de retorno Nothing,
+isso significa que ele nunca retorna (sempre lança uma exceção).
+*/
+fun nothing(): Nothing {
+    TODO("Retorna uma exceção")
 }
